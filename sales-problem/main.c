221f6742f689362b7438943183119a29ce9c0ea0
eisenburg <isenburg-eliana@ku.edu>
2024-03-06 11:52:59 -0600

    added sales summary

--- sales-problem/main.c
+++ sales-problem/main.c
@@ -21,15 +21,66 @@ void get_monthly_data(unsigned int earnings[NUM_MONTHS])
     }
 }
 
-void print_monthly_report(char months[NUM_MONTHS][MONTH_LENGTH], unsigned int earnings[])
+void print_monthly_report(char months[NUM_MONTHS][MONTH_LENGTH], unsigned int earnings[NUM_MONTHS])
 {
-    printf("Monthly sales report for 2023:\n");
+    printf("\nMonthly sales report for 2023:");
     for (int i = 0; i < NUM_MONTHS; i++)
     {
-        printf("\n%9s  ", months[i]);
+        printf("\n%-9s ", months[i]);
         print_dollars(earnings[i]);
     }
+    printf("\n\n");
 }
+
+unsigned int get_average_sales(int start_month, int end_month, unsigned int earnings[NUM_MONTHS])
+{
+    unsigned int total_sum = 0;
+    for (int m = start_month; m < end_month; m++)
+    {
+        total_sum += earnings[m];
+    }
+    return (total_sum / (end_month - start_month));
+}
+
+int find_min_index(unsigned int earnings[NUM_MONTHS])
+{
+    int current_min_index = 0;
+    for (int i = 0; i < NUM_MONTHS; i++)
+    {
+        if (earnings[i] < earnings[current_min_index])
+            current_min_index = i;
+    }
+    return current_min_index;
+}
+
+int find_max_index(unsigned int earnings[NUM_MONTHS])
+{
+    int current_max_index = 0;
+    for (int i = 0; i < NUM_MONTHS; i++)
+    {
+        if (earnings[i] > earnings[current_max_index])
+            current_max_index = i;
+    }
+    return current_max_index;
+}
+
+void print_sales_report(char months[NUM_MONTHS][MONTH_LENGTH], unsigned int earnings[NUM_MONTHS])
+{
+    printf("Sales summary:\n");
+    printf("Minimum sales:  ");
+    int super_month = find_min_index(earnings);
+    print_dollars(earnings[super_month]);
+    printf("  (%s)\n", months[super_month]);
+    printf("Maximum sales:  ");
+    super_month = find_max_index(earnings);
+    print_dollars(earnings[super_month]);
+    printf("  (%s)\n", months[super_month]);
+    printf("Average sales:  ");
+    print_dollars(get_average_sales(0, 12, earnings));
+    printf("\n");
+}
+
+void print_rolling_averages() {}
 
 int main()
 {
@@ -37,5 +88,7 @@ int main()
     unsigned int monthly_earnings_cents[12];
     get_monthly_data(monthly_earnings_cents);
     print_monthly_report(months, monthly_earnings_cents);
+    print_sales_report(months, monthly_earnings_cents);
+    print_rolling_averages(months, monthly_earnings_cents);
     return 0;
 }
